9ed6745841a1470bebe80706e8574e565c9bffe1
Jieff <jstarcaleb>
2022-12-21 22:47:42 +0000

    js

--- script/website.js
+++ script/website.js
@@ -3,7 +3,7 @@ const navbar = document.querySelector(".navbar")
 const logout = document.querySelector(".text-el6")
 const social = document.querySelector(".text-el4")
 const test = document.querySelector(".text-el1")
-
+const specialist = document.querySelector(".text-el2")
 
 hamburgerEl.addEventListener("click", function(){
   navbar.classList.toggle("change");
@@ -19,6 +19,9 @@ social.addEventListener("click", function(){
   
 })
 test.addEventListener("click", function(){
-  console.log("clicked")
   window.open('https://www.cdc.gov/diabetes/takethetest/', '_blank').focus();
+})
+
+specialist.addEventListener("click",function(){
+  window.open('https://www.icliniq.com/ask-a-doctor-online/diabetologist/diabetes', '_blank').focus();
 })
