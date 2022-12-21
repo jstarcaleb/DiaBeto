e864c8da2ba2fee2e84a4995565fda3114d776ee
Jieff <jstarcaleb>
2022-12-21 22:39:18 +0000

    js

--- script/website.js
+++ script/website.js
@@ -2,6 +2,8 @@ const hamburgerEl = document.querySelector(".hamburger-menu")
 const navbar = document.querySelector(".navbar")
 const logout = document.querySelector(".text-el6")
 const social = document.querySelector(".text-el4")
+const test = document.querySelector(".text-el1")
+
 
 hamburgerEl.addEventListener("click", function(){
   navbar.classList.toggle("change");
@@ -13,7 +15,10 @@ logout.addEventListener("click", function(){
 })
 
 social.addEventListener("click", function(){
-  console.log("clicked")
   window.open('https://github.com/jstarcaleb', '_blank').focus();
   
+})
+test.addEventListener("click", function(){
+  console.log("clicked")
+  window.open('https://www.cdc.gov/diabetes/takethetest/', '_blank').focus();
 })
