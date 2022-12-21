b10ccc0eee653eea554c31a7f521c40056ebdbea
Jieff <jstarcaleb>
2022-12-21 13:17:09 +0000

    website

--- script/website.js
+++ script/website.js
@@ -1,7 +1,7 @@
 const hamburgerEl = document.querySelector(".hamburger-menu")
 const navbar = document.querySelector(".navbar")
 const logout = document.querySelector(".text-el6")
-
+const social = document.querySelector(".text-el4")
 
 hamburgerEl.addEventListener("click", function(){
   navbar.classList.toggle("change");
@@ -10,4 +10,10 @@ hamburgerEl.addEventListener("click", function(){
 
 logout.addEventListener("click", function(){
   document.location.href="index.html"
+})
+
+social.addEventListener("click", function(){
+  console.log("clicked")
+  window.open('https://github.com/jstarcaleb', '_blank').focus();
+  
 })
