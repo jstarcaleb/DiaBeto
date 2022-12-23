c0fd8412a75b07d1a3f72e307280aad8ba1a73ff
Jieff <jstarcaleb>
2022-12-23 01:23:25 +0000

    js

--- script/sign-up.js
+++ script/sign-up.js
@@ -4,15 +4,30 @@ function registration(){
   var mail =document.getElementById("email-el").value
   var userPwd = document.getElementById("password-el").value
   var userName = document.getElementById("userName").value
-  
-
-
+ 
   localStorage.setItem('firstName', first)
   localStorage.setItem('lastName', last)
   localStorage.setItem('username', userName )
   localStorage.setItem('email-address',mail)
   localStorage.setItem('userPassword', userPwd)
+  document.location.href='index.html'
+  const fn = document.getElementById("first-name").value
+  const ln = document.getElementById("last-name").value
+  const un = document.getElementById("userName").value
+  const em = document.getElementById("email-el").value
+  const pw = document.getElementById("password-el").value
+  
 }
+  
+
+  
+
+ 
+  
+
+  
+  
+
 
 const Toggle = document.getElementById("toggle-button")
 const Pass = document.getElementById("password-el")
